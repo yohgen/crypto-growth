4159068dfc0f1d1aa3fde59121ec17b40ad46ef3
yohgen <jorgentau@gmail.com>
2021-05-26 18:24:00 +0300

    added Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        <a href='https://yohgen.netlify.app' target='_blank' rel='noopener noreferrer'>
+          Jørgen Tau
+        </a>{' '}
+        © 2021
+      </p>
+      <p>
+        Source code hosted on&nbsp;
+        <a href='https://github.com/yohgen/crypto-growth' target='_blank' rel='noopener noreferrer'>
+          GitHub
+        </a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
